c8e711a94e17e5529d40eb14faad9f03f27ce393
Andrius <35608576+andriusmw@users.noreply.github.com>
2022-06-24 10:08:29 +0200

    creado pages HomePage de prueba

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { Header} from "./components/Header"
 import { Footer} from "./components/Footer"
 import { Routes, Route, Link} from "react-router-dom"
+import {HomePage} from "./pages/HomePage"
+
 
 function App() {
   return (
@@ -10,7 +12,7 @@ function App() {
     <Header />
      <main>
        <Routes>
-          <Route path="/" element={<p>Estamos en el home</p>}  /> 
+          <Route path="/" element={<HomePage />}  /> 
           <Route path="/register" element={<p>formulario registro</p>}  /> 
         
        </Routes>
