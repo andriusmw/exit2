a3736be719cc4bcd11724cbd26715dc73a46212c
Andrius <35608576+andriusmw@users.noreply.github.com>
2022-06-24 11:03:13 +0200

    listo para empezar get
    mirar minuto 58:00 vídeo clase 9

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {HomePage} from "./pages/HomePage"
 import {EntryPage} from "./pages/EntryPage"
 import { LoginPage} from "./pages/LoginPage"
 import { RegisterPage} from "./pages/RegisterPage"
+import { NotFoundPage } from './pages/NotFoundPage';
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           <Route path="/register" element={<RegisterPage/> }  /> 
           <Route path="/login" element={<LoginPage />  }  /> 
           <Route path="/entry/:id" element={<EntryPage /> }  /> 
+          <Route path="*" element={<NotFoundPage/> }  /> 
         
        </Routes>
     
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom"
+
 export const Auth = () => {
     return(
         <ul>
-            <li>Register</li>
-            <li>Login</li>
+            <li> <Link to="/register">Register</Link> </li>
+            <li> <Link to="/login">Login</Link> </li>
+         
         </ul>
     )
 }
