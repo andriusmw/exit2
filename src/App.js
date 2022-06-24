e8bf8e4ecf73e2968e85897ce1fb186ad2f665ac
Andrius <35608576+andriusmw@users.noreply.github.com>
2022-06-24 10:20:11 +0200

    añadidas mas paginas de prueba, register, login, entryPage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Header} from "./components/Header"
 import { Footer} from "./components/Footer"
 import { Routes, Route, Link} from "react-router-dom"
 import {HomePage} from "./pages/HomePage"
+import {EntryPage} from "./pages/EntryPage"
+import { LoginPage} from "./pages/LoginPage"
+import { RegisterPage} from "./pages/RegisterPage"
 
 
 function App() {
@@ -13,7 +16,9 @@ function App() {
      <main>
        <Routes>
           <Route path="/" element={<HomePage />}  /> 
-          <Route path="/register" element={<p>formulario registro</p>}  /> 
+          <Route path="/register" element={<RegisterPage/> }  /> 
+          <Route path="/login" element={<LoginPage />  }  /> 
+          <Route path="/entry/:id" element={<EntryPage /> }  /> 
         
        </Routes>
     
