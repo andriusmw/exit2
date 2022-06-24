3fd7f6c66e4b5bcf7ab45ca5dadae5134f9aa99c
Andrius <35608576+andriusmw@users.noreply.github.com>
2022-06-24 16:57:29 +0200

    Habemos Login
    solucionado problema de loguear con la contraseña mal en backend.
    De haber tenido feedback del proyecto podríamos haberlo solucionado antes.

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,8 +1,43 @@
+import { useState } from "react";
+import { loginUserService } from "../services";
+
 export const LoginPage = () => {
+    //Controlamos los estados de los input
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [error,setError] = useState("");
+
+    const handleForm = async (e) => {
+        e.preventDefault();
+        setError("");
+        try {
+            const data = await loginUserService({email,password});
+
+            console.log(data);
+        } catch (error) {
+            setError(error.message)
+        }
+    }
+
     return (
     <section>
         <h1>Login</h1>
-        <p>Aquí irá el formulario de Login</p>
+       <form onSubmit={handleForm} >
+           <fieldset>
+               <label htmlFor="email">Email: </label>
+               <input type="email" id="email" name="email" required onChange={(e) => setEmail(e.target.value)} />
+           </fieldset>
+           <fieldset>
+               <label htmlFor="password">Password: </label>
+               <input type="password" id="password" name="password" required onChange={(e) => setPassword(e.target.value)} />
+           </fieldset>
+
+
+           <button>Login</button>
+           {error ? <p>{error}</p> : null}
+       </form>
+
+
     </section>
     );
 };
--- src/services/index.js
+++ src/services/index.js
@@ -25,4 +25,24 @@ export const registerUserService = async ({name,email,password}) => {
     }
 };
 
+//------------------- LOGIN SERVICE -----------------
 
+export const loginUserService = async ({email,password}) => {
+    const response = await fetch(`${process.env.REACT_APP_BACKEND}/login`,
+    {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({email,password})
+    });
+
+    const json = await response.json();
+
+    if(!response.ok) {
+        throw new Error(json.message)
+    }
+
+    return json.data;
+
+};
