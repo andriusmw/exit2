2b224cc0f5eaa3924c9b0d04118fdb1d900b5d9d
Carlos Montesinos <carlosmontesinos1990@gmail.com>
2022-07-09 10:02:43 +0100

    header and footer changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,14 @@
-export const Footer =  () => {
-    return (
-        <footer>FOOTER</footer>
-    );
-}
+export const Footer = () => {
+  return (
+    <footer>
+      A Full-Stack App Created by{" "}
+      <a href="https://github.com/MontesinosR" target="_blank">
+        MontesinosR
+      </a>{" "}
+      and{" "}
+      <a href="https://github.com/andriusmw/" target="_blank">
+        andriusmw
+      </a>
+    </footer>
+  );
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,18 @@
-
 import { Auth } from "./Auth";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
 export const Header = () => {
-    
-
-    return (
-        <header>
-            <h1> <Link to="/">EXIT2</Link> </h1>
-            <nav>
-                <Auth />
-            </nav>
-        </header>
-    );
+  return (
+    <header>
+      <h1>
+        {" "}
+        <Link to="/">Accessible City App</Link>{" "}
+      </h1>
+      <nav>
+        <Auth />
+      </nav>
+    </header>
+  );
 };
-
-
